df1381c58e4b5a56e0759a228662ea161bc31ac2
rich0021 <kurabersayap002@gmail.com>
2022-06-09 18:55:20 +0700

    fix infinite loop menu rev 1

--- src/Pembeli.java
+++ src/Pembeli.java
@@ -7,21 +7,18 @@ public class Pembeli extends Obat {
     public void Menu() {
         try {
             Scanner inputUser = new Scanner(System.in);
-            Scanner myreader = new Scanner(this.obat);
-
-            int numList = 0;
-
             while(true) {
+                Scanner myreader = new Scanner(this.obat);
                 System.out.println("Selamat datang di Apotek Sauyunan\n");
                 super.readObat();
                 System.out.println("\nKetik 0 untuk keluar dari program");
                 System.out.println("");
                 System.out.print("Silahkan Masukan Angka Obat Yang Akan Dibeli : ");
                 int pilihanUser = inputUser.nextInt();
-                
-                while(myreader.hasNextLine()) {
-                    numList++;
+                int numList = 0;
+                while(true) {
                     String data = myreader.nextLine();
+                    numList++;
                     String[] split = data.split(",");
                     if(pilihanUser == numList) {
                         System.out.println("\nObat yang akan dibeli adalah:");
@@ -29,24 +26,35 @@ public class Pembeli extends Obat {
                         System.out.println("Nama obat             :" + split[0]);
                         System.out.println("Stok obat             :" + split[1]);
                         System.out.println("Tanggal expire obat   :" + split[2]);
-
                         boolean isBeli = getYesorNo("Apakah anda ingin membeli obat ini?");
-
                         if(isBeli) {
                             if (ValidasiStok(split[0])) {
+                                KurangiStok(split[0]);
                                 System.out.println("\nObat dibeli");
-                                numList = 0;
+                                if (!getYesorNo("Apakah anda ingin membeli obat lagi ?")) {
+                                    System.out.println("Terima Kasih");
+                                    myreader.close();
+                                    System.exit(0);
+                                } 
                                 break;
                             }else{
                                 System.out.println("\nObat Habis");
-                                numList = 0;
+                                if (!getYesorNo("Apakah anda ingin membeli obat lagi ?")) {
+                                    System.out.println("Terima Kasih");
+                                    myreader.close();
+                                    System.exit(0);
+                                } 
                                 break;
                             }
                         } else {
-                            System.out.println("Gajadi beli");
+                            System.out.println("Terima Kasih");
                             myreader.close();
                             System.exit(0);
                         }
+                    }else if(pilihanUser == 0){
+                        System.out.println("Terima Kasih");
+                        myreader.close();
+                        System.exit(0);
                     }
                 }
             }
